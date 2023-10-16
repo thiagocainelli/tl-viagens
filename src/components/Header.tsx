eb5a31674cb6f06817a46e7afe552500f380d2d6
Thiago Cainelli <thiagocainelli@gmail.com>
2023-10-16 13:59:11 -0300

    Created the Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Header () {
+    return (
+        <>
+            <header className="bg-sky-800 w-screen p-5 flex items-center justify-between flex-wrap text-white">
+                <Link href="/">TL VIAGENS</Link>
+                <nav className="flex gap-7 flex-wrap">
+                    <Link href="/" className="border-b border-transparent hover:border-white">INÍCIO</Link>
+                    <Link href="#about" className="border-b border-transparent hover:border-white">A EMPRESA</Link>
+                    <Link href="#services" className="border-b border-transparent hover:border-white">SERVIÇOS</Link>
+                    <Link href="#contacts" className="border-b border-transparent hover:border-white">CONTATOS</Link>
+                </nav>
+        </header>
+        </>
+    )
+}
